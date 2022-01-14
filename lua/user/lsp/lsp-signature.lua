c5c489736b6c31511feb051d71d10003ecfaed9d
namnh <namnh.ftu@gmail.com>
2022-01-14 23:41:07 +0700

    change gitsigns and lsp icons

--- lua/user/lsp/lsp-signature.lua
+++ lua/user/lsp/lsp-signature.lua
@@ -15,18 +15,18 @@ local cfg = {
   -- This setting only take effect in insert mode, it does not affect signature help in normal
   -- mode, 10 by default
 
-  floating_window = false, -- show hint in a floating window, set to false for virtual text only mode
+  floating_window = true, -- show hint in a floating window, set to false for virtual text only mode
 
   floating_window_above_cur_line = true, -- try to place the floating above the current line when possible Note:
   -- will set to true when fully tested, set to false will use whichever side has more space
   -- this setting will be helpful if you do not want the PUM and floating win overlap
-  fix_pos = false, -- set to true, the floating window will not auto-close until finish all parameters
+  fix_pos = true, -- set to true, the floating window will not auto-close until finish all parameters
   hint_enable = true, -- virtual hint enable
-  hint_prefix = "  ", -- Panda for parameter
-  hint_scheme = "Comment",
+  hint_prefix = " ", -- Panda for parameter
+  hint_scheme = "String",
   use_lspsaga = false, -- set to true if you want to use lspsaga popup
   hi_parameter = "LspSignatureActiveParameter", -- how your parameter will be highlight
-  max_height = 12, -- max height of signature floating_window, if content is more than max_height, you can scroll down
+  max_height = 22, -- max height of signature floating_window, if content is more than max_height, you can scroll down
   -- to view the hiding contents
   max_width = 120, -- max_width of signature floating_window, line will be wrapped if exceed max_width
   handler_opts = {
