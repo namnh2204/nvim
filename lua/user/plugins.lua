970cf43733becc603d89b5787cf3635eb0042434
nam.nguyen14 <nam.nguyen14@mservice.com.vn>
2022-03-06 00:27:20 +0700

    add tmux navigator plugin

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -434,6 +434,9 @@ return packer.startup(function(use)
     end,
   }
 
+  use {
+    "christoomey/vim-tmux-navigator"
+  }
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
